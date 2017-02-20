04f10dd6d2ffc0ed5dabdd5b6ee7ae57d50964e2
wdq <1215971936@qq.com>
2017-02-20 15:29:26 +0800

    房计划

---
+++ js/public.js
@@ -0,0 +1,57 @@
+/*
+*下拉选择框
+* */
+$(function () {
+    $(".select-box").hover(function () {
+        $(this).find("ul").show();
+    }, function () {
+        $(this).find("ul").hide();
+    });
+
+    $(".select-box ul li").click(function () {
+        var selectVlaue=$(this).attr("value");
+        var selectText=$(this).html();
+
+        var obj=$(this).parents(".select-box");
+
+        obj.find(".form-control").html(selectText);
+        obj.find("input").val(selectVlaue);
+        obj.find("ul").hide();
+    });
+});
+
+
+/*
+ *电话号码验证
+ * @phone 手机号码
+ * */
+function is_mobile(phone) {
+    var flag = false;
+//    var reg0 = /^(([0\+]\d{2,3}-)?(0\d{2,3})-)?(\d{7,8})(-(\d{3,}))?$/;   //判断 固话
+    var reg1 = /^((\(\d{2,3}\))|(\d{3}\-))?(13|15|18|14|17)\d{9}$/;                     //判断 手机
+//    if (reg0.test(phone)) {
+//        flag = true;
+//    }
+    if (reg1.test(phone)) {
+        flag = true;
+    }
+    if (!flag) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/*
+ *邮箱格式验证
+ * @emails 邮箱地址
+ * */
+function is_email(emails) {
+    var reg = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+    var email = emails.match(reg);
+    if (email != null) {
+        return true;  //正确
+    } else {
+        return false; //错误
+    }
+}
