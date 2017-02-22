b2438b6670d547898aba24c769d65dcbed17179c
wdq <1215971936@qq.com>
2017-02-22 09:40:03 +0800

    房计划公共样式修改

---
+++ js/public/register.js
@@ -0,0 +1,24 @@
+/**
+ * Created by wdq on 17/2/22.
+ */
+/*
+ * 获取验证码倒计时
+ * */
+var wait = 60;
+var t;
+function times() {
+    if (wait == 0) {
+        $("#send").removeClass("disabled");
+        $("#send").html("获取验证码");
+        wait = 60;
+        clearTimeout(t);
+    } else {
+        //$('#send').unbind("click");
+        $("#send").addClass("disabled");
+        $("#send").html(wait+"秒后重发");
+        wait--;
+        t = setTimeout(function() {
+            times()
+        }, 1000)
+    }
+}
