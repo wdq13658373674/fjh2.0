bee5dc7723c0479cd81f96a9f1ba60680b12c24b
wdq <1215971936@qq.com>
2017-07-04 14:54:09 +0800

    查看详情

--- js/public/house.js
+++ js/public/house.js
@@ -1,11 +1,8 @@
 /**
- * 定制房：我要报名，我要定制公共js
+ * 定制房：我要报名
  */
 $(function () {
-    /*
-     * 1、我要报名
-     * 选择支付方式
-     * */
+    /* 选择支付方式 */
     $('.pay-list>li').click(function () {
         $(this).addClass('active').siblings().removeClass('active');
     });
@@ -19,35 +16,6 @@ $(function () {
         var inx = $(this).index();
         page.eq(inx).addClass('active').siblings().removeClass('active');
         sliders(inx);//sliders
-    });
-
-    /*
-     * 2、我要定制
-     * 选定此房源
-     * */
-    $('.table .select-btn').click(function () {
-        $('.table tr').removeClass('active');
-        $(this).parents('tr').addClass('active');
-    });
-
-    /*房源表tab切换*/
-    sliders2();
-    $('.menu-tab2 .tab').click(function () {
-        var inx = $(this).data('value')
-            , mark = $('.house-mark .mark')
-            , num = $('.house-num-list li')
-            , slider = $('.num-slider .flex-control-nav>li a');
-
-
-        /*楼栋超出滚动*/
-        var n = parseInt(inx / 6);
-        $('#thumbs .flex-control-nav>li a').eq(n).click();
-        $('.num-slider .flex-control-nav>li a').eq(inx).click();
-
-        /*连带样式*/
-        num.eq(inx).addClass('active').siblings().removeClass('active');
-        mark.eq(inx).addClass('active').siblings().removeClass('active');
-        sliders2();
     });
 })
 
@@ -80,27 +48,3 @@ function sliders(inx) {
     });
 }
 
-/*选择楼栋房源sliders*/
-function sliders2() {
-    $('#thumbs').flexslider({
-        prevText: '',
-        nextText: '',
-        animation: "slide",
-        controlNav: true,
-        animationLoop: false,
-        maxItems: 6,
-        directionNav: true,
-        slideshow: false,
-        itemWidth: 62,
-        itemMargin: 0
-    });
-    $('.num-slider').flexslider({
-        prevText: '',
-        nextText: '',
-        animation: "slide",
-        controlNav: true,
-        directionNav: false,
-        animationLoop: false,
-        slideshow: false
-    });
-}
