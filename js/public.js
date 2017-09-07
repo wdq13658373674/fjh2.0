9cf86aa1257304857bc6a8b30a01775d0877ee28
Andy <191142742@qq.com>
2017-09-07 09:35:02 +0800

    我的认筹详情 继续优惠调整

--- js/public.js
+++ js/public.js
@@ -65,3 +65,23 @@ function is_email(emails) {
         return false; //错误
     }
 }
+/**
+ * 格式化金额
+ * @param s  金额
+ * @param n  小数保留位数
+ * @returns {string}
+ */
+
+function fmoney(s, n)
+{
+    n = n > 0 && n <= 20 ? n : 2;
+    s = parseFloat((s + "").replace(/[^\d\.-]/g, "")).toFixed(n) + "";
+    var l = s.split(".")[0].split("").reverse(),
+        r = s.split(".")[1];
+    t = "";
+    for(i = 0; i < l.length; i ++ )
+    {
+        t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "");
+    }
+    return t.split("").reverse().join("") + "." + r;
+}
